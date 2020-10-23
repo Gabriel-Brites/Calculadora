650796a71b1efcace93c1fc9bc3df8e9ee9fd803
Gabriel-Brites <majorbiel@gmail.com>
2020-10-23 15:42:49 -0300

    acrescentadoo funções de subtração, multiplicação e divisão

--- calc.js
+++ calc.js
@@ -3,5 +3,26 @@ function somar() {
     var num2 = document.querySelector(".num2").value;
 
     var resultado = parseInt(num1) + parseInt(num2);
-    document.querySelector(".resultado").innerHTML = resultado;
+    document.querySelector(".resSoma").innerHTML = resultado;
+}
+function subtrair() {
+    var num1 = document.querySelector(".num1").value;
+    var num2 = document.querySelector(".num2").value;
+
+    var resultado = parseInt(num1) - parseInt(num2);
+    document.querySelector(".resSubtracao").innerHTML = resultado;
+}
+function multiplicar() {
+    var num1 = document.querySelector(".num1").value;
+    var num2 = document.querySelector(".num2").value;
+
+    var resultado = parseInt(num1) * parseInt(num2);
+    document.querySelector(".resMultiplicacao").innerHTML = resultado;
+}
+function dividir() {
+    var num1 = document.querySelector(".num1").value;
+    var num2 = document.querySelector(".num2").value;
+
+    var resultado = parseInt(num1) / parseInt(num2);
+    document.querySelector(".resDivisao").innerHTML = resultado;
 }
