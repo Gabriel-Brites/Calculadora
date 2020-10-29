0f6405953498aff692c038196d16d7d66099d49d
Gabriel-Brites <majorbiel@gmail.com>
2020-10-29 15:38:33 -0300

    ajustes das funções

--- calc.js
+++ calc.js
@@ -1,9 +1,4 @@
-var soma = function (a,b) {return a + b};
-var sub = function (a, b) {return a - b};
-var mult = function (a,b) {return a * b};
-var div = function (a,b) {return a / b };
-
-module.exports.soma = soma;
-module.exports.sub = sub;
-module.exports.mult = mult;
-module.exports.div = div;
+function soma (a,b) {return a+b};
+function sub (a,b) {return a-b};
+function mult (a,b) {return a*b};
+function div (a,b) {return a/b};
--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-const calc = require('./calc.js');
+const calc = require('./calc');
 
-console.log(calc.soma(2,3));
-console.log(calc.sub(2,3));
-console.log(calc.mult(2,3));
-console.log(calc.div(2,3));
-
+module.exports = calc;
