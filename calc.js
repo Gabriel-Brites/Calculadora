159edc1736adb48635a80288fad38f8048b850cd
Gabriel-Brites <majorbiel@gmail.com>
2020-10-29 15:50:25 -0300

    finalizado

--- calc.js
+++ calc.js
@@ -1,4 +1,9 @@
-function soma (a,b) {return a+b};
-function sub (a,b) {return a-b};
-function mult (a,b) {return a*b};
-function div (a,b) {return a/b};
+function soma (a,b) {return a + b};
+function sub (a, b) {return a - b};
+function mult (a,b) {return a * b};
+function div (a,b) {return a / b };
+
+module.exports.soma = soma;
+module.exports.sub = sub;
+module.exports.mult = mult;
+module.exports.div = div;
